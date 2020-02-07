922ebdeda54292270f14f333314851fba4dcfbdd
lalala <iuyangdaxia@163.com>
2020-02-07 16:12:36 +0800

    04-头部和页脚公共组件完成

--- components/header.vue
+++ components/header.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>头部组件</div>
+  <div class="header-wrapper">
+<!-- 用el-row结合flex布局能解决flex的兼容问题 -->
+<el-row type="flex" justify="space-between" align="middle" class="header">
+      <!-- 图片 -->
+      <div class="logo">
+          <img src="/logo.jpg" alt="">
+      </div>
+      <!-- 菜单 -->
+       <el-row type="flex" class="nav">
+          <nuxt-link to="/">首页</nuxt-link>
+          <nuxt-link to="/post">旅游攻略</nuxt-link>
+          <nuxt-link to="/hotel">酒店</nuxt-link>
+          <nuxt-link to="/air">国内机票</nuxt-link>
+      </el-row>
+      <!-- 用户中心 -->
+      <div>
+          <nuxt-link to="/user/login">登录/注册</nuxt-link>
+      </div>
+  </el-row>
+  </div>
 </template>
 
 <script>
@@ -8,6 +27,51 @@ export default {
 }
 </script>
 
-<style>
+<style scoped lang="less">
+.header-wrapper{
+    height: 60px;
+    background-color: #fff;
+    border-bottom: 1px solid #eee;
+    box-shadow: 0 3px 2px #f6f6f6;
+    min-width: 1000px;
+}
+.header{
+    width: 1000px;
+    height: 60px;
+    margin:0 auto;
+
+     .logo{
+        img{
+            width: 156px;
+            height:42px;
+            display: block;
+        }
+    }
+}
+.nav{
+    flex:1;
+    margin-left: 20px;
+       a{
+            display: block;
+            height: 60px;
+            line-height: 60px;
+            box-sizing: border-box;
+            padding: 0 20px;
+
+            &:hover{
+                border-bottom: 5px #409eff solid;
+                color: #409eff;
+            }
+        }
+    
+        .nuxt-link-exact-active{
+        background: #409eff;
+        color: #fff;
+            
+            &:hover {
+                color:#fff;
+                }
+        }          
+}
 
 </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,43 @@
 <template>
   <div>
+     <!-- 头部组件 -->
+    <Header></Header>
     <!-- nuxt就相当于<router-view> -->
     <nuxt />
+    <!-- 页脚组件 -->
+    <Footer></Footer>
+    
   </div>
 </template>
 
-<style>
+<script>
+// 引入头部和尾部组件
+import Header from "@/components/header.vue"
+import Footer from "@/components/footer.vue"
+  export default {
+  // 注册组件
+  components:{
+    Header,Footer
+  }
+}
+</script>
 
+<style>
+*{
+  margin:0;
+  padding:0;
+}
+ul,li,ol{
+  list-style: none;
+}
+a{
+  color:inherit;
+  text-decoration: none;
+}
+a:hover{
+  color:inherit
+}
+em,i{
+  font-style: none
+}
 </style>
